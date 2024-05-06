1e89ce4934dd9cf40d265f66a9c45a8c6a76865f
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2024-05-06 22:47:24 +0530

    feat: implement infinite scrolling

--- src/pages/JobsSearchPage.jsx
+++ src/pages/JobsSearchPage.jsx
@@ -1,10 +1,12 @@
-import { useEffect } from "react";
-import { Grid, Typography } from "@mui/material";
+import { useState, useEffect, useRef } from "react";
+import { Grid, Typography, Box } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchData } from "../redux/slices/jobsDataSlice";
 import JobPostingCard from "../components/JobPostingCard";
 
 const JobsSearchPage = () => {
+  const [offset, setOffset] = useState(0);
+  const observerTarget = useRef(null);
   const dispatch = useDispatch();
   const {
     data: { jdList: jobPostings, totalCount },
@@ -13,8 +15,31 @@ const JobsSearchPage = () => {
   } = useSelector((state) => state.jobsData);
 
   useEffect(() => {
-    dispatch(fetchData(10, 0));
-  }, []);
+    dispatch(fetchData(10, offset));
+
+    // Implement Infinite Scrolling
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting && jobPostings.length <= totalCount) {
+          setOffset((prev) => {
+            dispatch(fetchData(10, prev + 10));
+            return prev + 10;
+          });
+        }
+      },
+      { threshold: 0.5 }
+    );
+
+    if (observerTarget.current) {
+      observer.observe(observerTarget.current);
+    }
+
+    return () => {
+      if (observerTarget.current) {
+        observer.unobserve(observerTarget.current);
+      }
+    };
+  }, [observerTarget]);
 
   return (
     jobPostings?.length && (
@@ -28,6 +53,9 @@ const JobsSearchPage = () => {
               <JobPostingCard job={job} />
             </Grid>
           ))}
+          <Grid item ref={observerTarget}>
+            <Typography variant="body2">Loading more jobs</Typography>
+          </Grid>
         </Grid>
       </>
     )
