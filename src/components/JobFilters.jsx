4c4639fcb583b85463696c0407c04964a079a78c
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2024-05-08 10:32:52 +0530

    refactor: made UI responsive

--- src/components/JobFilters.jsx
+++ src/components/JobFilters.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { Autocomplete, Box, TextField } from "@mui/material";
+import {
+  Autocomplete,
+  Button,
+  Grid,
+  SwipeableDrawer,
+  TextField,
+} from "@mui/material";
 import { filters } from "../constants/filters";
 import {
   addFilter,
@@ -12,6 +18,7 @@ import { capitalizeSentence } from "../utils/helperUtils";
 const JobFilters = ({ appliedFilters }) => {
   const [location, setLocation] = useState("");
   const [companyName, setCompanyName] = useState("");
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
   const dispatch = useDispatch();
 
   const handleFilterChange = (id, value, reason) => {
@@ -26,56 +33,111 @@ const JobFilters = ({ appliedFilters }) => {
     }
   };
 
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setDrawerOpen(open);
+  };
+
   return (
-    <Box
-      sx={{ display: "flex", margin: "20px", flexWrap: "wrap", gap: "1rem" }}
-    >
-      {filters.map((eachFilter) => {
-        const { fieldName, id, isMultiSelect, selectionOptions, isGrouped } =
-          eachFilter;
-        return (
-          <Autocomplete
-            key={id}
-            id={id + "-filter"}
-            sx={{ minWidth: "10rem" }}
-            multiple={isMultiSelect}
-            options={selectionOptions}
-            groupBy={(option) => option.category}
-            getOptionLabel={(option) =>
-              capitalizeSentence(isGrouped ? option.option : option)
-            }
-            filterSelectedOptions
-            renderInput={(params) => (
-              <TextField {...params} label={fieldName} />
-            )}
-            onChange={(_, val, reason) => handleFilterChange(id, val, reason)}
-            disabled={
-              id === "location" &&
-              !!appliedFilters[id] &&
-              !Array.isArray(appliedFilters[id])
-            }
-          />
-        );
-      })}
-      <TextField
-        id="location-filter"
-        label="Location"
-        value={location}
-        onChange={(event) => {
-          setLocation(event.target.value);
-          handleFilterChange("location", event.target.value);
+    <>
+      <Button
+        sx={{
+          width: "100%",
+          color: "inherit",
+          fontSize: "1rem",
+          backgroundColor: "#55efc4ba",
+          "&:hover": {
+            backgroundColor: "primary.main",
+          },
+          marginBottom: "10px",
         }}
-      />
-      <TextField
-        id="companyName-filter"
-        label="Search Company Name"
-        value={companyName}
-        onChange={(event) => {
-          setCompanyName(event.target.value);
-          handleFilterChange("companyName", event.target.value);
-        }}
-      />
-    </Box>
+        onClick={toggleDrawer(true)}
+      >
+        Toggle Filters
+      </Button>
+      <SwipeableDrawer
+        anchor="bottom"
+        open={isDrawerOpen}
+        onClose={toggleDrawer(false)}
+      >
+        <Grid
+          container
+          sx={{
+            display: "flex",
+            padding: "20px",
+            flexWrap: "wrap",
+            gap: "1rem",
+          }}
+        >
+          {filters.map((eachFilter) => {
+            const {
+              fieldName,
+              id,
+              isMultiSelect,
+              selectionOptions,
+              isGrouped,
+            } = eachFilter;
+            return (
+              <Grid item xs={12} md={3}>
+                <Autocomplete
+                  key={id}
+                  id={id + "-filter"}
+                  sx={{ minWidth: "10rem" }}
+                  multiple={isMultiSelect}
+                  options={selectionOptions}
+                  groupBy={(option) => option.category}
+                  getOptionLabel={(option) =>
+                    capitalizeSentence(isGrouped ? option.option : option)
+                  }
+                  filterSelectedOptions
+                  renderInput={(params) => (
+                    <TextField {...params} label={fieldName} />
+                  )}
+                  onChange={(_, val, reason) =>
+                    handleFilterChange(id, val, reason)
+                  }
+                  disabled={
+                    id === "location" &&
+                    !!appliedFilters[id] &&
+                    !Array.isArray(appliedFilters[id])
+                  }
+                />
+              </Grid>
+            );
+          })}
+          <Grid item xs={12} md={3}>
+            <TextField
+              fullWidth
+              id="location-filter"
+              label="Location"
+              value={location}
+              onChange={(event) => {
+                setLocation(event.target.value);
+                handleFilterChange("location", event.target.value);
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <TextField
+              fullWidth
+              id="companyName-filter"
+              label="Search Company Name"
+              value={companyName}
+              onChange={(event) => {
+                setCompanyName(event.target.value);
+                handleFilterChange("companyName", event.target.value);
+              }}
+            />
+          </Grid>
+        </Grid>
+      </SwipeableDrawer>
+    </>
   );
 };
 
